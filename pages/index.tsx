695b6f31d085eae5ccff2f2e184167ebe56b9a05
Joaco <montesano1994@gmail.com>
2023-04-20 23:36:56 -0300

    reseteo contador cuando lista vuelve a cero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-
 import {useState } from "react";
 import {ITask, Priority, Status, statusOrder, priorityOrder} from '../types/ITask';
 import Task from '../components/TaskComponent';
+
 import ReactPaginate from 'react-paginate';
 
 
@@ -15,22 +15,19 @@ export default function TodoApp() {
   const [taskIdCounter, setTaskIdCounter] = useState<number>(0);
   const [priorityChoosen, setPriorityChoosen] = useState<Priority>(Priority.NonSelect);
   const [statusChoosen, setStatusChoosen] = useState<Status>(Status.NonSelect);
-
   const [currentPage, setCurrentPage] = useState(0);
 
   const handlePageChange = (selectedItem: { selected: number }) => {
-  setCurrentPage(selectedItem.selected);
-};
+    setCurrentPage(selectedItem.selected);
+  };
 
-const tasksPerPage = 2;
-const startIndex = currentPage * tasksPerPage;
-const endIndex = startIndex + tasksPerPage;
-
-const displayedTasks = tasks.slice(startIndex, endIndex);
+  const tasksPerPage = 2;
+  const startIndex = currentPage * tasksPerPage;
+  const endIndex = startIndex + tasksPerPage;
+  const displayedTasks = tasks.slice(startIndex, endIndex);
 
 
   const handleAddTask = () => {
-
     if (!title || priority === Priority.NonSelect || status === Status.NonSelect) {
       // list of fields not ok
       let fields = [];
@@ -65,108 +62,110 @@ const displayedTasks = tasks.slice(startIndex, endIndex);
     setStatus(Status.NonSelect);
     setTaskIdCounter(taskIdCounter + 1); // incremento el id
     setCharCount(0);
-  };
+    };
 
-  const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    setDescription(value);
-    setCharCount(value.length);
-  };
+    const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const value = e.target.value;
+      setDescription(value);
+      setCharCount(value.length);
+    };
 
 
-  const editTask = (priority: Priority, status: Status, taskId: number): void => {
-    setTasks(prevTasks => {
-      return prevTasks.map(task => { 
-        if (task.id === taskId) { // si es el id que se edita
-          return {
-            ...task,
-            priority,
-            status,
-          };
-        }
-        return task; // si no es el id que se edita, no hago nada
+    const editTask = (priority: Priority, status: Status, taskId: number): void => {
+      setTasks(prevTasks => {
+        return prevTasks.map(task => { 
+          if (task.id === taskId) { // si es el id que se edita
+            return {
+              ...task,
+              priority,
+              status,
+            };
+          }
+          return task; // si no es el id que se edita, no hago nada
+        });
       });
+    };
+
+  function sortById(): void {
+    const sortedTasks = [...tasks].sort((a, b) => {
+      return a.id < b.id ? -1 : 1;
     });
-  };
+    setTasks(sortedTasks);
+    setPriorityChoosen(Priority.NonSelect);
+    setStatusChoosen(Status.NonSelect);
+  }
 
-function sortById(): void {
-  const sortedTasks = [...tasks].sort((a, b) => {
-    return a.id < b.id ? -1 : 1;
-  });
-  setTasks(sortedTasks);
-  setPriorityChoosen(Priority.NonSelect);
-  setStatusChoosen(Status.NonSelect);
-}
+  function sortTasksByPriority(priorityChoosen: Priority): void {
+    const sortedTasks = [...tasks].sort((a, b) => {
+      // getter del index
+      const aIndex = priorityOrder[a.priority];
+      const bIndex = priorityOrder[b.priority];
 
-function sortTasksByPriority(priorityChoosen: Priority): void {
-  const sortedTasks = [...tasks].sort((a, b) => {
-    // getter del index
-    const aIndex = priorityOrder[a.priority];
-    const bIndex = priorityOrder[b.priority];
-
-    // si elijo Media, las tareas con media prioridad van primero
-    if (priorityChoosen === Priority.Media) {
-      if (a.priority === Priority.Media && b.priority !== Priority.Media) {
-        return -1;
-      } else if (a.priority !== Priority.Media && b.priority === Priority.Media) {
-        return 1;
+      // si elijo Media, las tareas con media prioridad van primero
+      if (priorityChoosen === Priority.Media) {
+        if (a.priority === Priority.Media && b.priority !== Priority.Media) {
+          return -1;
+        } else if (a.priority !== Priority.Media && b.priority === Priority.Media) {
+          return 1;
+        }
       }
-    }
-    // misma prioridad? ordenar por id
-    if (aIndex === bIndex) {
-      return a.id < b.id ? -1 : 1;
-    }
-
-    // si elijo Baja. ordeno ascendente
-    if (priorityChoosen === Priority.Baja) {
-      return aIndex - bIndex;
-    } else {
-      // Sino descendente (Alta)
-      return bIndex - aIndex;
-    }
-  });
-  setTasks(sortedTasks);
-}
-
-
-function sortTasksByStatus(statusChoosen: Status): void {
-  const sortedTasks = [...tasks].sort((a, b) => {
-    // getter del index
-    const aIndex = statusOrder[a.status];
-    const bIndex = statusOrder[b.status];
-
-    // si elijo EnProceso, las tareas EnProceso van primero
-    if (statusChoosen === Status.EnProceso) {
-      if (a.status === Status.EnProceso && b.status !== Status.EnProceso) {
-        return -1;
-      } else if (a.status !== Status.EnProceso && b.status === Status.EnProceso) {
-        return 1;
+      // misma prioridad? ordenar por id
+      if (aIndex === bIndex) {
+        return a.id < b.id ? -1 : 1;
       }
-    }
 
-    // misma prioridad? ordenar por id
-    if (aIndex === bIndex) {
-      return a.id < b.id ? -1 : 1;
-    }
+      // si elijo Baja. ordeno ascendente
+      if (priorityChoosen === Priority.Baja) {
+        return aIndex - bIndex;
+      } else {
+        // Sino descendente (Alta)
+        return bIndex - aIndex;
+      }
+    });
+    setTasks(sortedTasks);
+  }
 
-    // si elijo Finalizada. ordeno ascendente
-    if (statusChoosen === Status.Finalizada) {
-      return aIndex - bIndex;
-    } else {
-      // Sino descendente
-      return bIndex - aIndex;
-    }
-  });
 
-  setTasks(sortedTasks);
-}
+  function sortTasksByStatus(statusChoosen: Status): void {
+    const sortedTasks = [...tasks].sort((a, b) => {
+      // getter del index
+      const aIndex = statusOrder[a.status];
+      const bIndex = statusOrder[b.status];
+
+      // si elijo EnProceso, las tareas EnProceso van primero
+      if (statusChoosen === Status.EnProceso) {
+        if (a.status === Status.EnProceso && b.status !== Status.EnProceso) {
+          return -1;
+        } else if (a.status !== Status.EnProceso && b.status === Status.EnProceso) {
+          return 1;
+        }
+      }
+
+      // misma prioridad? ordenar por id
+      if (aIndex === bIndex) {
+        return a.id < b.id ? -1 : 1;
+      }
+
+      // si elijo Finalizada. ordeno ascendente
+      if (statusChoosen === Status.Finalizada) {
+        return aIndex - bIndex;
+      } else {
+        // Sino descendente
+        return bIndex - aIndex;
+      }
+    });
+    setTasks(sortedTasks);
+  }
 
 
 
   // filtro los que no coinciden con el id
   const handleDeleteTask = (taskId: number): void => {
     setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId)); 
-  }
+    if (tasks.length === 0) {
+      setTaskIdCounter(0); //resetea el id counter
+    }
+  
   
 
 return (
